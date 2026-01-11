e3fd5fe5340686321810248020ede1480c8f1bab
iliakazennov <iliakazennov@ya.ru>
2026-01-12 01:26:32 +0300

    add all adaptors

--- tests/core/adaptors/_init_.py
+++ tests/core/adaptors/_init_.py
@@ -5,5 +5,9 @@
 from .LoginAdaptor import LoginAdaptor
 from .RegisterAdaptor import RegisterAdaptor
 from .ProfileAdaptor import ProfileAdaptor
+from .ProductsAdaptor import ProductsAdaptor
+from .CartAdaptor import CartAdaptor
+from .FavoritesAdaptor import FavoritesAdaptor
+from .AdminProductsAdaptor import AdminProductsAdaptor
 
-__all__ = ['LoginAdaptor', 'RegisterAdaptor', 'ProfileAdaptor']
+__all__ = ['LoginAdaptor', 'RegisterAdaptor', 'ProfileAdaptor', 'ProductsAdaptor', 'CartAdaptor', 'FavoritesAdaptor', 'AdminProductsAdaptor']
